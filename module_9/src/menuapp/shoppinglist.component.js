74a22fd8b9031c9f17e1a74a2fec69324d92205f
Christopher Paul <christopherpaul@Christophers-MacBook-Pro.local>
2023-02-14 22:35:09 -0500

    update 2

--- module_9/src/menuapp/shoppinglist.component.js
+++ module_9/src/menuapp/shoppinglist.component.js
@@ -3,7 +3,7 @@
 
 angular.module('MenuApp')
 .component('shoppingList', {
-  templateUrl: 'src/shoppinglist/templates/shoppinglist.template.html',
+  templateUrl: 'src/menuapp/templates/shoppinglist.template.html',
   bindings: {
     items: '<'
   }
