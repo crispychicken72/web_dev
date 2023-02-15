02e4ab89b30946fd66fa2e2d03e0eef08f790150
Christopher Paul <christopherpaul@Christophers-MacBook-Pro.local>
2023-02-14 21:39:13 -0500

    saving progress

---
+++ module_9copy2/src/shoppinglist/shoppinglist.service.js
@@ -0,0 +1,25 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingList')
+.service('ShoppingListService', ShoppingListService);
+
+
+ShoppingListService.$inject = ['$q', '$timeout', '$http']
+function ShoppingListService($q, $timeout, $http) {
+  var service = this;
+
+  service.getAllCategories = function() {
+      return $http({
+          method: "GET",
+          url: "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json"
+      }).then(function (resp) {
+          console.log("service.getAllCategories");
+          console.log(resp.data);
+          
+          return resp.data;
+      });
+  };
+}
+
+})();
